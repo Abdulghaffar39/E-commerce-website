a7a9762924c6b9e1c2465613ab4cfb27bf89d999
abdulghaffar39 <abdulghaffar15a@gmail.com>
2026-02-25 19:41:33 +0500

    working backend

--- Backend/Controller/productController.js
+++ Backend/Controller/productController.js
@@ -1,4 +1,7 @@
-const productSchema = require("../DB/DB_Product")
+// const productSchema = require("../DB/DB_Product")
+// const { findOne } = require("../DB/DB_Schema")
+
+const productSchema = require("../DB/DB_Product");
 
 
 
@@ -10,14 +13,18 @@ async function postProduct(req, res) {
 
         const AllProducts = { ProductName, BrandName, Category, Description, Price, StockQuantity, BatteryCapacity, DisplayType, StrapMaterial, WaterResistance, Warranty }
 
+
+
         if (!AllProducts) {
             return res.send({
                 status: 201,
                 message: "Product empty"
             })
         }
-
+        
         const Products = await new productSchema(AllProducts).save()
+        
+        console.log(Products);
 
         return res.send({
 
@@ -35,4 +42,80 @@ async function postProduct(req, res) {
 }
 
 
-module.exports = { postProduct }
+async function getProduct(req, res) {
+
+    try {
+
+        const get = await productSchema.findOne()
+
+        return res.send({
+
+            status: 200,
+            message: "Product Get Successfuly"
+        })
+
+    }
+    catch (err) {
+        res.send({
+            status: 400,
+            message: "product not get"
+        })
+    }
+}
+
+
+async function updateProduct(req, res) {
+
+    const { id } = req.params;
+
+    console.log(id);
+
+    const { ProductName, BrandName, Category, Description, Price, StockQuantity, BatteryCapacity, DisplayType, StrapMaterial, WaterResistance, Warranty } = req.body
+
+    try {
+
+        const updated = await productSchema.findByIdAndUpdate(id, { ProductName, BrandName, Category, Description, Price, StockQuantity, BatteryCapacity, DisplayType, StrapMaterial, WaterResistance, Warranty })
+        console.log(updated);
+
+        if (!updated) {
+            return res.status(404).json({ success: false, message: 'Product not found' });
+        }
+
+        res.status(200).json({
+
+            success: true,
+            message: 'Product updated successfully',
+            data: updated,
+        });
+
+    }
+    catch (err) {
+        res.send({
+            status: 400,
+            message: "product not get"
+        })
+    }
+}
+
+async function deleteProduct(req, res) {
+
+    try {
+        const { id } = req.params;
+        console.log(id);
+
+        res.status(200).json({
+            success: true,
+            message: 'Product deleted successfully',
+        });
+
+    }
+    catch (err) {
+        res.send({
+            status: 400,
+            message: "product not get"
+        })
+    }
+}
+
+
+module.exports = { postProduct, getProduct, updateProduct, deleteProduct }
--- Backend/Router/routes.js
+++ Backend/Router/routes.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const { signUp, login } = require("../Controller/auth");
-const { postProduct } = require("../Controller/productController");
+const { postProduct, getProduct, updateProduct, deleteProduct } = require("../Controller/productController");
 
 const router = express.Router();
 
 router.post("/signUp", signUp)
 router.post("/login", login)
 router.post("/postProduct", postProduct)
+router.get("/getProduct", getProduct)
+router.put(`/updateProduct/:id`, updateProduct)
+router.delete("/deleteProduct/:id", deleteProduct)
 
 
 
